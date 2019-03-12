c6ee7c349311cf06973df454fa99c6810cc750f5
KoingDev <sea.saingkoing@gmail.com>
2019-03-12 23:33:12 +0700

    Fix indent

--- MockNetworkingSampleTests/MockNetworkingSampleTests.swift
+++ MockNetworkingSampleTests/MockNetworkingSampleTests.swift
@@ -32,13 +32,13 @@ class MockNetworkingSampleTests: XCTestCase {
 
 	let mockWebService = MockWebService()
 	
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+	override func setUp() {
+		// Put setup code here. This method is called before the invocation of each test method in the class.
+	}
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+	override func tearDown() {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+	}
 
 	func testGetAllTodosSuccess() {
 		// Given
