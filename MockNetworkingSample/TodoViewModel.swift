2d9d0ed1e23aaa93ed97b2ff8f45e1e4bbf9ff7f
KoingDev <sea.saingkoing@gmail.com>
2019-03-12 23:34:25 +0700

    Remove comment

--- MockNetworkingSample/TodoViewModel.swift
+++ MockNetworkingSample/TodoViewModel.swift
@@ -8,12 +8,6 @@
 
 import Foundation
 
-////////////////////////////////////////////////////////////////
-//MARK: -
-//MARK: WITH D.I
-//MARK: -
-////////////////////////////////////////////////////////////////
-
 final class TodoViewModel {
 	
 	let webService: WebServiceType
@@ -23,34 +17,6 @@ final class TodoViewModel {
 	init(webService: WebServiceType = WebService()) {
 		self.webService = webService
 	}
-	
-	func getAllTodos(completion: @escaping (Bool) -> Void) {
-		webService.getAllTodos { status, todos in
-			switch status {
-			case .success:
-				// does something here for success...
-				self.todos = todos
-				completion(true)
-			case .fail:
-				// does something here for fail...
-				self.todos = []
-				completion(false)
-			}
-		}
-	}
-	
-}
-
-////////////////////////////////////////////////////////////////
-//MARK: -
-//MARK: WITHOUT D.I (HOW TO MOCK ???) 🤔
-//MARK: -
-////////////////////////////////////////////////////////////////
-
-final class TodoViewModelWithoutDI {
-	
-	let webService = WebService()
-	var todos: [Todo]!
 	
 	func getAllTodos(completion: @escaping (Bool) -> Void) {
 		webService.getAllTodos { status, todos in
